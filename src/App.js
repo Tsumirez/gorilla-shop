d1e226f7d2e74a8e88fafc62776e67b0609496f7
Tsumirez <tsumirez@zoho.com>
2021-07-28 23:05:55 +0200

    Added component directory structure + actual components for displaying homepage.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./homepage.component";
+import HomePage from "./pages/homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <Homepage></Homepage>
+      <HomePage></HomePage>
     </div>
   );
 }
