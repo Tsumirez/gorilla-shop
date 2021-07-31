4f224ac3432785b7a014d681ebb8195fdfb5b173
Tsumirez <tsumirez@zoho.com>
2021-07-31 20:14:16 +0200

    Hooked up authentication functionality to SignIn and Header components.
    Header now has SIGN IN or SIGN OUT button based on user login state.
    SignIn component has had some extra styling applied to better fit both sign in buttons.
    Button component got bit of extra code to display styling based on extra prop.

--- src/components/collection-item/header-component.jsx
+++ src/components/collection-item/header-component.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './header-component.styles.scss';
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header-component.styles.scss";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
-let HeaderComponent = () => (
+let HeaderComponent = ({ currentUser }) => {
+  console.log(currentUser);
+  return (
     <div className="header">
-        <Link to='/' className='logo-container'>
-            <Logo className='logo' />
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
         </Link>
-        <div className="options">
-            <Link className="option" to="/shop">SHOP</Link>
-            <Link className="option" to="/contact">CONTACT</Link>
-        </div>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={()=>auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+      </div>
     </div>
-);
+  );
+};
 
 export default HeaderComponent;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../button/button.component";
 
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor() {
@@ -50,9 +50,14 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <CustomButton type="submit">Sumbit Form</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
-
+          <div className="buttons">
+            <CustomButton type="submit">
+              SIGN IN WITH EMAIL
+            </CustomButton>
+            <CustomButton isGoogleSignIn={true} onClick={signInWithGoogle}>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
