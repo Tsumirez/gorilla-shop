5706912b0d847cb22f1239119abad1f629706915
Tsumirez <tsumirez@zoho.com>
2021-07-31 19:26:58 +0200

    Google authetication added to App's state.
    Things got real. Wheather a user is loggged in or not is an app-wide information.
    So app.js got rehauled into class component to hold the state.
    
    Additionally two lifecycle methods are added - Component did Mount connects to Google firebase auth
    and sets up a way to close the constantly open connection.
    
    That job falls to componentWillUnmount, so no memory leak is left in form of connection go firebase while app is already gone.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,43 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInUp from "./components/sign-in-out/sign-in-up.component";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInUp} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./firebase/firebase.utils";
+import { render } from "@testing-library/react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
