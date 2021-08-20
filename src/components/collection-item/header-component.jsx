0ee39ae8e0d50f766178b601f06cd19a29d04599
Tsumirez <tsumirez@zoho.com>
2021-08-20 12:40:01 +0200

    Hooked up redux store user data to Header component.
    import { connect } from "react-redux";
    
    const mapStateToProps = state =>({
      currentUser: state.user.currentUser
    })
    
    export default connect(mapStateToProps)(HeaderComponent);
    
    These lines connect Header with currentUser stored in redux store.
    
    connect() is a higher order component function that modifies passed component (header here)
    to be receving props from store, which are determined in mapStateToProps function.

--- src/components/collection-item/header-component.jsx
+++ src/components/collection-item/header-component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import "./header-component.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
@@ -32,4 +33,8 @@ let HeaderComponent = ({ currentUser }) => {
   );
 };
 
-export default HeaderComponent;
+const mapStateToProps = state =>({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(HeaderComponent);
